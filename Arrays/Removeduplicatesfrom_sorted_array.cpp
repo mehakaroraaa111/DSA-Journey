d28fa0adfa3d7f70144729b3ddb56b062d627b2a
Mehak <mehakaroraaa111@gmail.com>
2026-06-08 14:46:14 +0530

    Add removeDuplicates function for sorted array
    Implemented the removeDuplicates function using the two-pointer technique to modify the input vector in place.

---
+++ Arrays/Removeduplicatesfrom_sorted_array.cpp
@@ -0,0 +1,21 @@
+// LeetCode 26 - Remove Duplicates from Sorted Array
+// Approach: Two Pointer Technique
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        int i = 0;
+        for (int j = 1; j< nums.size(); j++){
+            if (nums[j] != nums[i]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+};
